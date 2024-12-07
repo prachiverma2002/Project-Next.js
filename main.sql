6a868962f92127c73b0b85565dec9fecb60a91fa
prachiverma2002 <95541735+prachiverma2002@users.noreply.github.com>
2024-12-07 23:24:12 +0530

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,10 @@
+CREATE TABLE schools (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name TEXT,
+    address TEXT,
+    city TEXT,
+    state TEXT,
+    contact BIGINT,
+    image TEXT,
+    email_id TEXT
+);
